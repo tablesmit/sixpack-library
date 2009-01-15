22c56e1fae0203d127067188b9f8bc5b99e06f54
aaubry <aaubry@25845490-acef-11dd-9a14-c1645b63b0c6>
2009-01-15 20:09:59 +0000

    Removed SixPack.Aop

--- personal/aaubry/src/SixPack.Validation.PostSharp.Installer/PostsharpRequired.cs
+++
@@ -1,4 +0,0 @@
-﻿// Include this file in projects that use PostSharp to ensure that they are not compiled without it.
-#if !POSTSHARP
-#error You need to install Postsharp to be able to compile this project correctly.
-#endif
