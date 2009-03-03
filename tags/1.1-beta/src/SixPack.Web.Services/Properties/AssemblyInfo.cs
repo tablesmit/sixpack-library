458242a500fc458c6f04087eefaab4d4e6c3077c
marco.cecconi <marco.cecconi@25845490-acef-11dd-9a14-c1645b63b0c6>
2009-03-03 12:25:16 +0000

    1.1 beta

---
+++ tags/1.1-beta/src/SixPack.Web.Services/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+// AssemblyInfo.cs 
+//
+//  Copyright (C) 2008 Fullsix Marketing Interactivo LDA
+//  Author: Marco Cecconi
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+//
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly : AssemblyTitle("SixPack.Web.Services")]
+#if DEBUG
+[assembly : AssemblyDescription("SixPack Library Web.Services - Debug build")]
+#else
+[assembly : AssemblyDescription("SixPack Library Web.Services - Release build")]
+#endif
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly : AssemblyVersion("1.0.0.*")]
+[assembly : AssemblyFileVersion("1.0.0.1")]
---
+++ tags/1.1-beta/src/SixPack/Diagnostics/TimerSample.cs
@@ -0,0 +1,108 @@
+// TimerSample.cs 
+//
+//  Copyright (C) 2008 Fullsix Marketing Interactivo LDA
+//  Author: Marco Cecconi
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+//
+
+using System;
+using System.Globalization;
+
+namespace SixPack.Diagnostics
+{
+	/// <summary>
+	/// Value that can be used to calculate a time interval.
+	/// </summary>
+	public struct TimerSample
+	{
+		internal readonly long Value;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TimerSample"/> struct.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		public TimerSample(long value)
+		{
+			Value = value;
+		}
+
+		/// <summary>
+		/// Indicates whether this instance and a specified object are equal.
+		/// </summary>
+		/// <param name="obj">Another object to compare to.</param>
+		/// <returns>
+		/// true if <paramref name="obj"/> and this instance are the same type and represent the same value; otherwise, false.
+		/// </returns>
+		public override bool Equals(object obj)
+		{
+			if(obj.GetType() != typeof(TimerSample))
+			{
+				return false;
+			}
+			return Equals((TimerSample)obj);
+		}
+
+		/// <summary>
+		/// Returns the hash code for this instance.
+		/// </summary>
+		/// <returns>
+		/// A 32-bit signed integer that is the hash code for this instance.
+		/// </returns>
+		public override int GetHashCode()
+		{
+			return Value.GetHashCode();
+		}
+
+		/// <summary>
+		/// Returns the fully qualified type name of this instance.
+		/// </summary>
+		/// <returns>
+		/// A <see cref="T:System.String"/> containing a fully qualified type name.
+		/// </returns>
+		public override string ToString()
+		{
+			return Value.ToString(CultureInfo.InvariantCulture);
+		}
+
+		/// <summary>
+		/// Indicates whether this instance and a specified object are equal.
+		/// </summary>
+		/// <param name="obj">Another object to compare to.</param>
+		/// <returns>
+		/// true if <paramref name="obj"/> and this instance are the same type and represent the same value; otherwise, false.
+		/// </returns>
+		public bool Equals(TimerSample obj)
+		{
+			return obj.Value == Value;
+		}
+		
+		/// <summary>
+		/// Implements the operator ==.
+		/// </summary>
+		public static bool operator ==(TimerSample left, TimerSample right)
+		{
+			return left.Equals(right);
+		}
+
+		/// <summary>
+		/// Implements the operator !=.
+		/// </summary>
+		public static bool operator !=(TimerSample left, TimerSample right)
+		{
+			return !left.Equals(right);
+		}
+	}
+}
